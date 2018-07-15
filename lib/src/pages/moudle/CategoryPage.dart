d67d112fb533a25f7c7e64bbb2d1dfa53b223c35
Android_Fan <3213696630@qq.com>
2018-07-15 15:31:10 +0800

    【func】：实现下拉刷新和上拉加载更多

--- lib/src/pages/moudle/CategoryPage.dart
+++ lib/src/pages/moudle/CategoryPage.dart
@@ -1,6 +1,73 @@
 import 'package:flutter/material.dart';
-class CategoryPage extends StatelessWidget {
 
+import 'dart:async' show Future, Timer;
+import '../../utils/refresh/Refresh.dart';
+
+class CategoryPage extends StatefulWidget {
+  CategoryPage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  CategoryPageState createState() => new CategoryPageState();
+}
+
+class CategoryPageState extends State<CategoryPage> {
+  Key key = GlobalKey();
+  int _counter = 20;
+
+  Future<Null> getData() {
+    setState(() {
+      _counter += 10;
+      if (_counter > 40) {
+        isnomore = true;
+      }
+    });
+    return Future.delayed(Duration(milliseconds: 500), () {
+
+    });
+  }
+
+  Future<Null> reset() {
+    setState(() {
+      list.clear();
+      _counter = 20;
+      isnomore = false;
+    });
+    return Future.delayed(Duration(milliseconds: 500), () {
+
+    });
+  }
+
+  bool isnomore = false;
+
+  final List<String> list = [];
+
+  List<Widget> _listBuilder(int i) {
+    final List<Widget> listw = [];
+    if (list.isEmpty) {
+      for (int i = 0; i < 1000; i++) {
+        list.add("Item " + i.toString());
+      }
+    }
+    for (int count = 0; count < i; count++) {
+      listw.add(
+          ListTile(leading: Icon(Icons.favorite_border, color: Colors.red,),
+              title: Text(list[count])));
+    }
+    listw.add(Padding(padding: EdgeInsets.only(bottom: 16.0),
+        child: Center(child: Text(!isnomore ? "" : '我是有底线的'))),);
+    return listw;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -8,8 +75,28 @@ class CategoryPage extends StatelessWidget {
 
     return new Scaffold(
       body: new Center(
-        child:
-        new Text("分类"),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Expanded(
+                child: RefreshLayout(canloading: !isnomore, onRefresh: (boo) {
+                  if (!boo) {
+                    return getData();
+                  } else {
+                    return reset();
+                  }
+                }, key: key,
+                    child: new ListView(children: _listBuilder(_counter),)))
+            ,
+            new Text(
+              '$_counter',
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .display1,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/src/pages/moudle/HomePage.dart
+++ lib/src/pages/moudle/HomePage.dart
@@ -1,18 +1,67 @@
 import 'package:flutter/material.dart';
 
 import '../../utils/ToastImpl.dart';
+import 'dart:convert';
+import 'dart:io';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new HomePageState();
+  }
+}
 
+class HomePageState extends State<HomePage> {
+  var _ipAddress = 'Unknown';
+
+  _getIPAddress() async {
+    var url = 'http://test.ylfood.com/b2cnew-web/resource/ads/get_home_page_info?width=750&height=366&limit=10';
+    var httpClient = new HttpClient();
+
+    String result;
+    try {
+      var request = await httpClient.getUrl(Uri.parse(url));
+      var response = await request.close();
+      if (response.statusCode == HttpStatus.OK) {
+        var json = await response.transform(UTF8.decoder).join();
+        var data = JSON.decode(json);
+        result = data['result']['advertisementList'][0]['slideName'];
+      } else {
+        result =
+        'Error getting IP address:\nHttp status ${response.statusCode}';
+      }
+    } catch (exception) {
+      result = 'Failed getting IP address';
+    }
+
+    // If the widget was removed from the tree while the message was in flight,
+    // we want to discard the reply rather than calling setState to update our
+    // non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _ipAddress = result;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    var spacer = new SizedBox(height: 32.0);
 
     return new Scaffold(
       body: new Center(
-        child:
-          new Text("首页"),
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text('Your current IP address is:'),
+            new Text('$_ipAddress.'),
+            spacer,
+            new RaisedButton(
+              onPressed: _getIPAddress,
+              child: new Text('Get IP address'),
+            ),
+          ],
+        ),
       ),
     );
   }
